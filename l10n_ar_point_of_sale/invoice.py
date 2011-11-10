91ae53832afd50c80e4ecabd66c3abdb4187efc8
Samuel Fabrizio <sfabrizio@e-mips.com.ar>
2011-11-10 17:50:32 -0300

    Resolucion del Numero de la Factura
    [ADD]  Se agrego la carpeta l18n para mas adelante hacer las traducciones
    [ADD]  Se agrego la funcion 'action_number', la cual se hereda del objeto 'account.invoice'. Dicha funcion genera el numero de factura y lo guarda en el campo 'internal_number'
    [MOD] Modificaciones varias en la vista de la Fatura, las mas importante : el campo 'internal_number' reemplaza al campo 'number' el cual no se muestra mas.

--- l10n_ar_point_of_sale/invoice.py
+++ l10n_ar_point_of_sale/invoice.py
@@ -35,5 +35,77 @@ class invoice(osv.osv):
             ],'Type', readonly=True, select=True, change_default=True),
         'pos_ar_id' : fields.many2one('pos.ar','Point of Sale'),
         'denomination_id' : fields.many2one('invoice.denomination','Denomination', readonly=True),
+        'internal_number': fields.char('Invoice Number', size=32, readonly=False, help="Unique number of the invoice, computed automatically when the invoice is created."),
     }
+    
+    def action_number(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        #TODO: not correct fix but required a frech values before reading it.
+        self.write(cr, uid, ids, {})
+
+        for obj_inv in self.browse(cr, uid, ids, context=context):
+            id = obj_inv.id
+            invtype = obj_inv.type
+            number = obj_inv.number
+            move_id = obj_inv.move_id and obj_inv.move_id.id or False
+            reference = obj_inv.reference or ''
+            
+#del        self.write(cr, uid, ids, {'internal_number':number})
+#se agrega:         
+        #~ si el usuario no ingreso un numero, busco el ultimo y lo incremento , si no hay ultimo va 1. 
+        #~ si el usuario hizo un ingreso dejo ese numero
+            if not obj_inv.internal_number:
+                pos_ar = obj_inv.pos_ar_id.id
+                cr.execute("select max(to_number(internal_number, '99999999')) from account_invoice where internal_number ~ '^[0-9]+$' and pos_ar_id=%s and state in %s", (pos_ar, ('open', 'paid', 'cancel',)))
+                max_number = cr.fetchone()    
+                if not max_number[0]:
+                    val = "1"
+                    self.write(cr, uid, id, {'internal_number' : val })
+                    print 'hola ' , id
+                else :
+                    val = str( int(max_number[0]) + 1)
+                    self.write(cr, uid, id, {'internal_number' : val })
+            else :
+                self.write(cr, uid, id, {'internal_number' : obj_inv.internal_number })
+# fin de agregado            
+            if invtype in ('in_invoice', 'in_refund'):
+                if not reference:
+                    ref = self._convert_ref(cr, uid, number)
+                else:
+                    ref = reference
+            else:
+                ref = self._convert_ref(cr, uid, number)
+
+            cr.execute('UPDATE account_move SET ref=%s ' \
+                    'WHERE id=%s AND (ref is null OR ref = \'\')',
+                    (ref, move_id))
+            cr.execute('UPDATE account_move_line SET ref=%s ' \
+                    'WHERE move_id=%s AND (ref is null OR ref = \'\')',
+                    (ref, move_id))
+            cr.execute('UPDATE account_analytic_line SET ref=%s ' \
+                    'FROM account_move_line ' \
+                    'WHERE account_move_line.move_id = %s ' \
+                        'AND account_analytic_line.move_id = account_move_line.id',
+                        (ref, move_id))
+
+            for inv_id, name in self.name_get(cr, uid, [id]):
+                ctx = context.copy()
+                if obj_inv.type in ('out_invoice', 'out_refund'):
+                    ctx = self.get_log_context(cr, uid, context=ctx)
+                message = ('Invoice ') + " '" + name + "' "+ ("is validated.")
+                self.log(cr, uid, inv_id, message, context=ctx)
+        return True
+        
+    
+    def write(self, cr, uid, ids, vals, context=None):
+        
+        if 'internal_number' in vals.keys():
+            try: 
+                int(vals['internal_number']) 
+            except :
+                raise osv.except_osv( ('Error'),
+                                      ('The Invoice Number can not contain characters'))
+        return super(invoice, self).write(cr, uid, ids, vals, context=context)
+
 invoice()
