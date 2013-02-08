97b7b732044084e3231f832bf4e633425c68acec
Sebastian Kennedy <skennedy@e-mips.com.ar>
2013-02-08 20:31:20 -0300

    Initial commit. Modulo perteneciente a Eynes.
    Modulo creado por Laureano Kloss de Eynes.
    [ADD] Objeto Checkbook o chequera para manejo de cheques propios. Mejora
    el uso de cheques propios en account.voucher de pago a proveedores.
    [MOD] Extendido el objeto account.issued.check para el uso de chequeras.
    Vistas modificadas.
    [ADD] Vistas creadas para Chequeras

---
+++ l10n_ar_account_create_check/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2013 Eynes S.R.L. (http://www.eynes.com.ar) All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Checkbook Management",
+    "version" : "0.1",
+    "author" : "eynes.com.ar",
+    "website" : "www.eynes.com.ar",
+    "category" : "Localisation/Argentine",
+    "description": """Checkbook management for Own Checks""",
+    "license" : "AGPL-3",
+    "depends" : ["base","l10n_ar_account_check"],
+    "init_xml" : [],
+    "update_xml" : [
+        "checkbook_view.xml",
+        "wizard/create_checkbook_view.xml"
+        ],
+    "active": False,
+    "installable": True
+}
+
+
+
+
---
+++ l10n_ar_account_create_check/checkbook.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#                       Jordi Esteve <jesteve@zikzakmedia.com>
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class checkbook(osv.osv):
+    _name = "checkbook"
+    _description = "checkbook"
+
+    _columns = {
+        'bank_id': fields.many2one('res.partner.bank','Bank', required=True),
+        'name': fields.char('Number of check', size=20, required=True),
+        'checkbook_num': fields.char('Checkbook number', size=20, required=True),
+        'state': fields.char('State', size=40, required=True),
+    }
+    
+    _defaults = {
+		'state': 'draft'
+    }
+checkbook()
+
+class account_issued_check(osv.osv):
+    _inherit = 'account.issued.check'
+
+    _columns = {
+        'check_id': fields.many2one('checkbook', 'Check'),
+        'number': fields.char('Check Number', size=20),
+        }
+        
+    def on_change_check_id(self, cr, uid, ids, check_id, context=None):
+        if context is None:
+            context = {}
+        if not check_id:
+            return {'value':{}}
+
+        check = self.pool.get('checkbook').browse(cr, uid, check_id, context=context)
+        return {'value':{'account_bank_id': check.bank_id.id, 'bank_id': check.bank_id.bank.id, 'number': check.name}}
+        
+    def write(self, cr, uid, ids, vals, context=None):
+		a = vals.get('check_id', False)
+		if a:
+			sql = 'select check_id from account_issued_check where id = ' + str(ids[0])
+			cr.execute(sql)
+			aux_check_id = cr.fetchone()
+			self.pool.get('checkbook').write(cr, uid, aux_check_id, {'state': 'draft'})
+			self.pool.get('checkbook').write(cr, uid, a, {'state': 'done'})
+		return super(account_issued_check, self).write(cr, uid, ids, vals, context=context)
+
+    def create(self, cr, uid, vals, context=None):
+		a = vals.get('check_id', False)
+		if a:
+			self.pool.get('checkbook').write(cr, uid, a, {'state': 'done'})
+		return super(account_issued_check, self).create(cr, uid, vals, context=context)
+		
+    def unlink(self, cr, uid, ids, context=None):
+		sql = 'select check_id from account_issued_check where id = ' + str(ids[0])
+		cr.execute(sql)
+		aux_check_id = cr.fetchone()
+		self.pool.get('checkbook').write(cr, uid, aux_check_id, {'state': 'draft'})
+		return super(account_issued_check, self).unlink(cr, uid, ids, context=context)
+        
+account_issued_check()
