c82677068db714ed39380bb9e38119404b0eb60e
Sebastian Kennedy <skennedy@e-mips.com.ar>
2013-08-24 19:09:32 -0300

    Creacion de modulo. Sobrecarga de hook_add_taxes
    refs #332

---
+++ l10n_ar_wsfe_perceptions/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2011
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+{
+    "name": "WSFE Perceptions",
+    "version": "1.0",
+    "depends": ["base", "l10n_ar_point_of_sale", "l10n_ar_perceptions_basic", "l10n_ar_perceptions", "l10n_ar_wsfe" ],
+    "author": "E-MIPS",
+    "website": "http://e-mips.com.ar",
+    "license": "GPL-3",
+    "category": "Own Modules",
+    "description": """
+    This module adds Perceptions to the detail of Electronic Invoice.
+    """,
+    "init_xml": [
+        ],
+    'update_xml': [
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
