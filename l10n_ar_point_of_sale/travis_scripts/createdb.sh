15fc971338bfd064a838465da1c70f58904851be
Sebastian Kennedy <skennedy@e-mips.com.ar>
2014-04-05 16:48:04 -0300

    Integracion continua con Travis CI (II)
    [MOD] Se modifica .travis.yml para agregar la instalacion de librerias
    de python por pip como aconseja Travis CI.
    [REM] Se borra el archivo install_dependencies.sh porque ya no se
    instalan paquetes con apt-get.
    [FIX] Se corrige el directorio de instalacion de OpenERP7.
    [MOD] Tambien se agregan las dependencias del modulo (base_vat_ar) y se
    hacen los link simbolicos para que OpenERP7 los pueda instalar.
    [MOD] Se agrega la creacion del usuario de OpenERP para postgres.
    [FIX] Se corrigen las lineas de comando para lanzar el OpenERP
    instalando los modulos necesarios para el testing.
    [ADD] Se agrega el archivo requirements.txt con las librerias a instalar
    con pip.
    [MOD] Se modifica createdb.sh para que utilice el usuario creado para la
    BD postgresql y un error de directorio.

--- l10n_ar_point_of_sale/travis_scripts/createdb.sh
+++ l10n_ar_point_of_sale/travis_scripts/createdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./openerp-server -r postgres &
-sleep 5
+$HOME/openerp-7.0/openerp-server --xmlrpc-port=8069 --logfile=$HOME/openerp.log -r openerp -w p4s5W0rD! &
+sleep 10
 OPENERP_PID=$!
-python ./create_db.py
+python travis_scripts/create_db.py
 sleep 10
 kill $OPENERP_PID
