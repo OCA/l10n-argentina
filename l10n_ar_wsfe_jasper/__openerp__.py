54e4b48f020506c018c77030381fcc50645268f3
Laureano Kloss <laureano.kloss@eynes.com.ar>
2015-01-01 19:28:52 -0300

    Commit inicial

---
+++ l10n_ar_wsfe_jasper/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2013 E-MIPS (http://www.e-mips.com.ar) All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "l10n_ar_wsfe_jasper",
+    "version": "2.0",
+    "depends": ["l10n_ar_wsfe"],
+    "author": "E-MIPS",
+    "website": "http://e-mips.com.ar",
+    "license": "GPL-3",
+    "category": "Argentina Localization",
+    "description": """
+        WSFE (Web Service de Factura Electronica).
+        Este módulo nos permite facturar de forma electrónica a través del Servicio Web
+        que publica la AFIP.
+    """,
+    'data': [
+        #~ 'wsfe_data.xml',
+        #~ 'wsfe_view.xml',
+        'account_invoice_view.xml',
+        #~ 'account_invoice_workflow.xml',
+        #~ 'wizard/wsfe_sinchronize_voucher_view.xml',
+    ],
+    'demo': [
+        #~ 'wsfe_demo.xml',
+        ],
+    'test': [
+        #~ 'test/invoice_customer.yml',
+        #~ 'test/refund_customer.yml',
+        #~ 'test/invoice_supplier.yml',
+        #~ 'test/invoice_massive.yml',
+        ],
+    'installable': True,
+    'active': False,
+}
---
+++ l10n_ar_wsfe_jasper/invoice.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2013 E-MIPS (http://www.e-mips.com.ar) All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+from datetime import datetime
+from tools.translate import _
+import pooler
+import time
+import re
+
+__author__ = "Sebastian Kennedy <skennedy@e-mips.com.ar>"
+
+class account_invoice(osv.osv):
+    _name = "account.invoice"
+    _inherit = "account.invoice"
+    
+    def _compute_bar_code(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for inv in self.browse(cr, uid, ids, context=context):
+            cuit = inv.company_id.partner_id.vat
+            pos = '0005'
+            
+            eivoucher_obj = self.pool.get('wsfe.voucher_type')
+            aux_res = eivoucher_obj.search(cr, uid, [('document_type', '=', inv.type), ('denomination_id', '=', inv.denomination_id.id)])[0]
+
+            ei_voucher_type = eivoucher_obj.browse(cr, uid, aux_res)
+            inv_code = ei_voucher_type.code
+
+            if inv.state == 'open' and inv.cae != 'NA' and inv.cae_due_date:
+                cae = inv.cae
+                cae_due_date = datetime.strptime(inv.cae_due_date, '%Y-%m-%d')
+            else:
+                cae_due_date = datetime.now()
+                cae = '0'*14
+
+            code = cuit+'%02d'%int(inv_code)+pos+cae+cae_due_date.strftime('%Y%m%d')+'4'
+            
+            res[inv.id] = code
+        return res
+
+    _columns = {
+        'bar_code': fields.function(_compute_bar_code, string='Bar code', type='char'),
+    }
+
+
+   
+
+account_invoice()
