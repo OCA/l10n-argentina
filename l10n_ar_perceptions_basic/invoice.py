83aef2f3ad8d89738be34b329af60eb91fe6aaf9
Sebastian Kennedy <skennedy@e-mips.com.ar>
2015-01-02 10:27:45 -0300

    Cambio key de account.tax. Agregado .gitignore
    [MOD] Se modifica la key usada en el calculo de impuestos por el codigo
    que se actualizo en Odoo que ya no utilizan dentro de la key la
    analytic_account_id

--- l10n_ar_perceptions_basic/invoice.py
+++ l10n_ar_perceptions_basic/invoice.py
@@ -192,7 +192,7 @@ class account_invoice_tax(osv.osv):
                 val['account_id'] = tax.account_paid_id.id
                 val['account_analytic_id'] = tax.account_analytic_paid_id.id
 
-            key = (val['tax_code_id'], val['base_code_id'], val['account_id'], val['account_analytic_id'])
+            key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
             if not key in tax_grouped:
                 tax_grouped[key] = val
             else:
