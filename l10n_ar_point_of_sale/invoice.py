dfde1b88d29f9e0f45ce88c3deb95efba2b3e043
Santiago J. Said <said.santiago@gmail.com>
2015-04-23 13:12:42 -0300

    [RM] Legacy code

--- l10n_ar_point_of_sale/invoice.py
+++ l10n_ar_point_of_sale/invoice.py
@@ -259,7 +259,6 @@ class invoice(models.Model):
 
     @api.multi
     def action_number(self):
-
         next_number = None
         invoice_vals = {}
 
@@ -422,86 +421,14 @@ class account_invoice_tax(models.Model):
 
         return {'value': val}
 
-    @api.v7
-    def hook_compute_invoice_taxes(self, cr, uid, invoice_id, tax_grouped, context=None):
-        return tax_grouped
-
     @api.v8
     def hook_compute_invoice_taxes(self, invoice, tax_grouped):
-        return tax_grouped
-
-    @api.v7
-    def compute(self, cr, uid, invoice_id, context=None):
-        tax_grouped = {}
-        tax_obj = self.pool.get('account.tax')
-        cur_obj = self.pool.get('res.currency')
-        inv = self.pool.get('account.invoice').browse(cr, uid, invoice_id, context=context)
-        cur = inv.currency_id
-        company_currency = inv.company_id.currency_id.id
-
-        ctx = context.copy()
-        if not 'date' in ctx:
-            ctx['date'] = inv.date_invoice or time.strftime('%Y-%m-%d')
-
-        for line in inv.invoice_line:
-            for tax in tax_obj.compute_all(cr, uid, line.invoice_line_tax_id, (line.price_unit* (1-(line.discount or 0.0)/100.0)), line.quantity, line.product_id, inv.partner_id)['taxes']:
-                val={}
-                # TODO: Tal vez se pueda cambiar el boolean is_exempt por un type (selection)
-                # para despues incluir impuestos internos, retenciones, percepciones, o sea,
-                # distintos tipos de impuestos
-                is_exempt = tax_obj.read(cr, uid, tax['id'], ['is_exempt'], context)['is_exempt']
-                val['invoice_id'] = inv.id
-                val['name'] = tax['name']
-                val['amount'] = tax['amount']
-                val['manual'] = False
-                val['sequence'] = tax['sequence']
-                val['is_exempt'] = is_exempt
-                val['base'] = tax['price_unit'] * line['quantity']
-                val['tax_id'] = tax['id']
-
-                if inv.type in ('out_invoice','in_invoice'):
-                    val['base_code_id'] = tax['base_code_id']
-                    val['tax_code_id'] = tax['tax_code_id']
-                    val['base_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['base'] * tax['base_sign'], context=ctx, round=False)
-                    val['tax_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['amount'] * tax['tax_sign'], context=ctx, round=False)
-                    val['account_id'] = tax['account_collected_id'] or line.account_id.id
-                    val['account_analytic_id'] = tax['account_analytic_collected_id']
-                else:
-                    val['base_code_id'] = tax['ref_base_code_id']
-                    val['tax_code_id'] = tax['ref_tax_code_id']
-                    val['base_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['base'] * tax['ref_base_sign'], context=ctx, round=False)
-                    val['tax_amount'] = cur_obj.compute(cr, uid, inv.currency_id.id, company_currency, val['amount'] * tax['ref_tax_sign'], context=ctx, round=False)
-                    val['account_id'] = tax['account_paid_id'] or line.account_id.id
-                    val['account_analytic_id'] = tax['account_analytic_paid_id']
-
-                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
-                if not key in tax_grouped:
-                    tax_grouped[key] = val
-                else:
-                    tax_grouped[key]['amount'] += val['amount']
-                    tax_grouped[key]['base'] += val['base']
-                    tax_grouped[key]['base_amount'] += val['base_amount']
-                    tax_grouped[key]['tax_amount'] += val['tax_amount']
-
-        for t in tax_grouped.values():
-            t['base'] = cur_obj.round(cr, uid, cur, t['base'])
-            t['amount'] = cur_obj.round(cr, uid, cur, t['amount'])
-            t['base_amount'] = cur_obj.round(cr, uid, cur, t['base_amount'])
-            t['tax_amount'] = cur_obj.round(cr, uid, cur, t['tax_amount'])
-
-        # Agregamos un hook por si otros modulos quieren agregar sus taxes
-        tax_grouped = self.hook_compute_invoice_taxes(cr, uid, invoice_id, tax_grouped, ctx)
-
         return tax_grouped
 
     @api.v8
     def compute(self, invoice):
         tax_grouped = {}
 
-        #tax_obj = self.pool.get('account.tax')
-        #cur_obj = self.pool.get('res.currency')
-
-        #inv = self.pool.get('account.invoice').browse(cr, uid, invoice_id, context=context)
         currency = invoice.currency_id.with_context(date=invoice.date_invoice or fields.Date.context_today(invoice))
         company_currency = invoice.company_id.currency_id
 
