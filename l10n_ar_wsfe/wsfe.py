8a83888135c5f48485618116dd784993d4d16fa6
Sebastian Kennedy <skennedy@e-mips.com.ar>
2015-05-05 17:41:55 -0300

    Bug fix cuando partner_id tiene parent_id

--- l10n_ar_wsfe/wsfe.py
+++ l10n_ar_wsfe/wsfe.py
@@ -173,9 +173,10 @@ class wsfe_config(osv.osv):
                     msg = 'Observaciones: ' + '\n'.join(comp['Observaciones'])
 
                 # Chequeamos que se corresponda con la factura que enviamos a validar
-                doc_type = inv.partner_id.document_type_id and inv.partner_id.document_type_id.afip_code or '99'
-                doc_tipo = comp['DocTipo'] == int(doc_type)
-                doc_num = comp['DocNro'] == int(inv.partner_id.vat)
+                partner = inv.partner_id.parent_id or inv.partner_id
+                partner_doc_type = partner.document_type_id and partner.document_type_id.afip_code or '99'
+                doc_tipo = comp['DocTipo'] == int(partner_doc_type)
+                doc_num = comp['DocNro'] == int(partner.vat)
                 cbte = True
                 if inv.internal_number:
                     cbte = comp['CbteHasta'] == int(inv.internal_number.split('-')[1])
@@ -367,8 +368,12 @@ class wsfe_config(osv.osv):
             detalle = {}
 
             fiscal_position = inv.fiscal_position
-            doc_type = inv.partner_id.document_type_id and inv.partner_id.document_type_id.afip_code or '99'
-            doc_num = inv.partner_id.vat or '0'
+            if inv.partner_id.parent_id:
+                doc_type = inv.partner_id.parent_id.document_type_id and inv.partner_id.parent_id.document_type_id.afip_code or '99'
+                doc_num = inv.partner_id.parent_id.vat or '0'
+            else:
+                doc_type = inv.partner_id.document_type_id and inv.partner_id.document_type_id.afip_code or '99'
+                doc_num = inv.partner_id.vat or '0'
 
             # Chequeamos si el concepto es producto, servicios o productos y servicios
             product_service = [l.product_id and l.product_id.type or 'consu' for l in inv.invoice_line]
