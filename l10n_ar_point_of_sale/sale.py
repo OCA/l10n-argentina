a1794cd923b40f83bce09fed2986f346cae241a2
Sebastian Kennedy <skennedy@e-mips.com.ar>
2015-05-25 15:54:41 -0300

    Se revierte parte de 5da0a445 del calculo de amounts en sale.order
    [REV] Se quitan los cambios que se habían hecho por el tema de los
    calculos en las sale.order de las cantidades totales, ya que ssaid ya
    habia hecho un modulo l10n_ar_sale_order con esa modificacion en
    particular.

--- l10n_ar_point_of_sale/sale.py
+++ l10n_ar_point_of_sale/sale.py
@@ -25,55 +25,9 @@ from openerp import models, fields, api, _
 from openerp.osv import osv
 import openerp.addons.decimal_precision as dp
 
-class sale_order_line(models.Model):
-    _name = "sale.order.line"
-    _inherit = "sale.order.line"
-
-    @api.one
-    @api.depends('price_unit', 'discount', 'tax_id', 'product_uom_qty',
-        'product_id', 'order_id.partner_id', 'order_id.currency_id')
-    def _amount_line_ar(self):
-        tax_obj = self.env['account.tax']
-
-        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-        taxes = tax_obj.compute_all(price, self.product_uom_qty, self.product_id, self.order_id.partner_id)
-        cur = self.order_id.pricelist_id.currency_id
-        self.price_subtotal = cur.round(taxes['total'])
-
-    # Columns
-    price_subtotal = fields.Float(string='Subtotal', digits=dp.get_precision('Account'),
-        store=False, readonly=True, compute='_amount_line_ar')
-
-sale_order_line()
-
 class sale_order(models.Model):
     _name = "sale.order"
     _inherit = "sale.order"
-
-    @api.one
-    @api.depends('order_line.price_subtotal',
-                 'order_line.tax_id',
-                 'order_line.discount')
-    def _amount_all_ar(self):
-        val = val1 = 0.0
-
-        cur = self.pricelist_id.currency_id
-
-        for line in self.order_line:
-            val1 += line.price_subtotal
-            val += self._amount_line_tax(line)
-
-        self.amount_tax = cur.round(val)
-        self.amount_untaxed = cur.round(val1)
-        self.amount_total = self.amount_untaxed + self.amount_tax
-
-
-    amount_untaxed = fields.Float(string='Untaxed Amount', digits=dp.get_precision('Account'),
-        store=True, readonly=True, compute='_amount_all_ar')
-    amount_tax = fields.Float(string='Taxes', digits=dp.get_precision('Account'),
-        store=True, readonly=True, compute='_amount_all_ar')
-    amount_total = fields.Float(string='Total', digits=dp.get_precision('Account'),
-        store=True, readonly=True, compute='_amount_all_ar')
 
     @api.v7
     def _get_pos_ar(self, cr, uid, order, denom_id, context=None):
@@ -107,21 +61,4 @@ class sale_order(models.Model):
 
         return invoice_id
 
-#    def action_wait(self, cr, uid, ids, *args):
-#        for o in self.browse(cr, uid, ids):
-#            if not o.fiscal_position:
-#                #TODO poner esto en un log:
-#                print 'Error - No Fiscal Position Setting. Please set the Fiscal Position First'
-#                #~ raise osv.except_osv(   _('No Fiscal Position Setting'),
-#                                        #~ _('Please set the Fiscal Position First'))
-#            if (o.order_policy == 'manual'):
-#                self.write(cr, uid, [o.id], {'state': 'manual', 'date_confirm': time.strftime('%Y-%m-%d')})
-#            else:
-#                self.write(cr, uid, [o.id], {'state': 'progress', 'date_confirm': time.strftime('%Y-%m-%d')})
-#            self.pool.get('sale.order.line').button_confirm(cr, uid, [x.id for x in o.order_line])
-#            message = _("The quotation '%s' has been converted to a sales order.") % (o.name,)
-#            self.log(cr, uid, o.id, message)
-#        return True
-
-    
 sale_order()
