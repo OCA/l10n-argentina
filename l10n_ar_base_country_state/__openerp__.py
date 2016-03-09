7be7cf64e65f371a09b51ad97c4cb418b932100a
Gabriel Davini <gabrielfranciscodavini@gmail.com>
2016-03-09 15:48:32 -0300

    [ADD] Add cities data file and object

--- l10n_ar_base_country_state/__openerp__.py
+++ l10n_ar_base_country_state/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "Base Argentina's States (aka Provinces)",
-    "version": "1.0",
+    "version": "1.1",
     "depends": ["base"],
     "author": "Gabriel Davini",
     "category": "Base Modules",
@@ -30,7 +30,12 @@
         * States (aka provinces) for Argentina.
     """,
     "init_xml": [],
-    'data': ['country_state_data.xml'],
+    'data': [
+        'data/country_state_data.xml',
+        'data/res_city_data.xml',
+        'views/partner_view.xml',
+        'views/res_city_view.xml',
+    ],
     'demo_xml': [],
     'installable': True,
     'application': True,
