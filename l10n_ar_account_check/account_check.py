7d7ff90dea46cdc301f2d0db0d444c1d7e7a9e20
Sebastian Kennedy <skennedy@e-mips.com.ar>
2016-03-23 11:45:15 -0300

    Se borran los cheques de un voucher borrado.
    [ADD] ondelete='cascade' a los cheques para que cuando se borre el
    voucher que los contiene, tambien se borren los cheques

--- l10n_ar_account_check/account_check.py
+++ l10n_ar_account_check/account_check.py
@@ -155,7 +155,7 @@ class account_third_check(models.Model):
     origin = fields.Char('Origin', size=64)
     dest = fields.Char('Destiny', size=64)
     deposit_bank_id = fields.Many2one('res.partner.bank', 'Deposit Account')
-    source_voucher_id = fields.Many2one('account.voucher', 'Source Voucher', readonly=True)
+    source_voucher_id = fields.Many2one('account.voucher', 'Source Voucher', ondelete="cascade", readonly=True)
     debit_note_id = fields.Many2one('account.invoice', 'Debit Note', readonly=True, help="In case of rejection of the third check")
     type = fields.Selection([('common', 'Common'), ('postdated', 'Post-dated')], 'Check Type', readonly=True, states={'draft': [('readonly', False)]}, default='common', help="If common, checks only have issued_date. If post-dated they also have payment date")
     note = fields.Text('Additional Information')
