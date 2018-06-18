ebc64236c211e5ae50f5aaf385dacb6284bd7adc
Sebastian Kennedy <skennedy@e-mips.com.ar>
2018-06-18 13:25:13 -0300

    [MOD] Patch recompute_voucher_lines method to add hooks

--- l10n_ar_account_payment/account_voucher.py
+++ l10n_ar_account_payment/account_voucher.py
@@ -23,6 +23,7 @@
 
 from osv import osv, fields
 from tools.translate import _
+from openerp.addons.account_voucher.account_voucher import account_voucher as account_voucher_orig
 
 class account_voucher(osv.osv):
 
@@ -322,20 +323,264 @@ class account_voucher(osv.osv):
 
         return True
 
+#    def recompute_voucher_lines(self, cr, uid, ids, partner_id, journal_id, price, currency_id, ttype, date, context=None):
+#
+#        print 'recompute_voucher_lines: ', __name__
+#
+#        default = super(account_voucher, self).recompute_voucher_lines(cr, uid, ids, partner_id, journal_id, price, currency_id, ttype, date, context=context)
+#
+#        ml_obj = self.pool.get('account.move.line')
+#
+#        for vals in default['value']['line_dr_ids']+default['value']['line_cr_ids']:
+#            if vals['move_line_id']:
+#                reads = ml_obj.read(cr, uid, vals['move_line_id'], ['invoice', 'ref'], context=context)
+#                vals['invoice_id'] = reads['invoice'] and reads['invoice'][0] or False
+#                vals['ref'] = reads['ref']
+#        return default
+
+    def _get_move_line_ids(self, cr, uid, voucher_ids, partner_id, account_type, context):
+        move_line_obj = self.pool.get('account.move.line')
+
+        if not context.get('move_line_ids', False):
+            ids = move_line_obj.search(cr, uid, [('state','=','valid'), ('account_id.type', '=', account_type), ('reconcile_id', '=', False), ('partner_id', '=', partner_id)], context=context)
+        else:
+            ids = context['move_line_ids']
+        return ids
+
+
+    def _get_move_lines_found(self, cr, uid, line, price, invoice_id,
+            currency_id, company_currency, move_lines_found, context=None):
+
+        to_break = False
+
+        if invoice_id:
+            if line.invoice.id == invoice_id:
+                #if the invoice linked to the voucher line is equal to the invoice_id in context
+                #then we assign the amount on that line, whatever the other voucher lines
+                move_lines_found.append(line.id)
+        elif currency_id == company_currency:
+            #otherwise treatments is the same but with other field names
+            if line.amount_residual == price:
+                #if the amount residual is equal the amount voucher, we assign it to that voucher
+                #line, whatever the other voucher lines
+                move_lines_found.append(line.id)
+                to_break = True
+            #otherwise we will split the voucher amount on each line (by most old first)
+        elif currency_id == line.currency_id.id:
+            if line.amount_residual_currency == price:
+                move_lines_found.append(line.id)
+                to_break = True
+
+        return to_break
+
+    def _get_voucher_line_vals(self, line, line_currency_id,
+            amount, amount_original, amount_unreconciled):
+
+        rs = {
+            'name':line.move_id.name,
+            'type': line.credit and 'dr' or 'cr',
+            'move_line_id':line.id,
+            'account_id':line.account_id.id,
+            'amount_original': amount_original,
+            'amount': amount,
+            'date_original':line.date,
+            'date_due':line.date_maturity,
+            'amount_unreconciled': amount_unreconciled,
+            'currency_id': line_currency_id,
+            'invoice_id': line.invoice.id,
+            'ref': line.ref,
+        }
+        return rs
+
+    def _try_autoimpute(self, rs, line, amount_unreconciled,
+            total_debit, total_credit):
+        if line.credit:
+            amount = min(amount_unreconciled, abs(total_debit))
+            rs['amount'] = amount
+            total_debit -= amount
+        else:
+            amount = min(amount_unreconciled, abs(total_credit))
+            rs['amount'] = amount
+            total_credit -= amount
+
+        return total_debit, total_credit
+
+    # PATCHED Method
     def recompute_voucher_lines(self, cr, uid, ids, partner_id, journal_id, price, currency_id, ttype, date, context=None):
+        """
+        Returns a dict that contains new values and context
 
-        default = super(account_voucher, self).recompute_voucher_lines(cr, uid, ids, partner_id, journal_id, price, currency_id, ttype, date, context=context)
+        @param partner_id: latest value from user input for field partner_id
+        @param args: other arguments
+        @param context: context arguments, like lang, time zone
 
-        ml_obj = self.pool.get('account.move.line')
+        @return: Returns a dict which contains new values, and context
+        """
+        def _remove_noise_in_o2m():
+            """if the line is partially reconciled, then we must pay attention to display it only once and
+                in the good o2m.
+                This function returns True if the line is considered as noise and should not be displayed
+            """
+            if line.reconcile_partial_id:
+                if currency_id == line.currency_id.id:
+                    if line.amount_residual_currency <= 0:
+                        return True
+                else:
+                    if line.amount_residual <= 0:
+                        return True
+            return False
 
-        for vals in default['value']['line_dr_ids']+default['value']['line_cr_ids']:
-            if vals['move_line_id']:
-                reads = ml_obj.read(cr, uid, vals['move_line_id'], ['invoice', 'ref'], context=context)
-                vals['invoice_id'] = reads['invoice'] and reads['invoice'][0] or False
-                vals['ref'] = reads['ref']
+        if context is None:
+            context = {}
+        context_multi_currency = context.copy()
+
+        currency_pool = self.pool.get('res.currency')
+        move_line_pool = self.pool.get('account.move.line')
+        partner_pool = self.pool.get('res.partner')
+        journal_pool = self.pool.get('account.journal')
+        line_pool = self.pool.get('account.voucher.line')
+
+        #set default values
+        default = {
+            'value': {'line_dr_ids': [] ,'line_cr_ids': [] ,'pre_line': False,},
+        }
+
+        #drop existing lines
+        line_ids = ids and line_pool.search(cr, uid, [('voucher_id', '=', ids[0])]) or False
+        if line_ids:
+            line_pool.unlink(cr, uid, line_ids)
+
+        if not partner_id or not journal_id:
+            return default
+
+        journal = journal_pool.browse(cr, uid, journal_id, context=context)
+        partner = partner_pool.browse(cr, uid, partner_id, context=context)
+        currency_id = currency_id or journal.company_id.currency_id.id
+
+        total_credit = 0.0
+        total_debit = 0.0
+        account_type = None
+        if context.get('account_id'):
+            account_type = self.pool['account.account'].browse(cr, uid, context['account_id'], context=context).type
+        if ttype == 'payment':
+            if not account_type:
+                account_type = 'payable'
+            total_debit = price or 0.0
+        else:
+            total_credit = price or 0.0
+            if not account_type:
+                account_type = 'receivable'
+
+        ids = self._get_move_line_ids(cr, uid, ids, partner_id, account_type, context=context)
+
+        invoice_id = context.get('invoice_id', False)
+        company_currency = journal.company_id.currency_id.id
+        move_lines_found = []
+
+        #order the lines by most old first
+        ids.reverse()
+        account_move_lines = move_line_pool.browse(cr, uid, ids, context=context)
+
+        # Differentiate lines by currency, so that, can be
+        # automatically applied only amounts on same currency
+        lines_same_currency = []
+        lines_diff_currency = []
+
+        #compute the total debit/credit and look for a matching open amount or invoice
+        for line in account_move_lines:
+            if _remove_noise_in_o2m():
+                continue
+
+            to_break = self._get_move_lines_found( cr, uid, line, price,
+                            invoice_id, currency_id, company_currency,
+                            move_lines_found, context=context)
+
+            line_currency_id = line.currency_id and \
+                    line.currency_id.id or company_currency
+
+            if currency_id == line_currency_id:
+                lines_same_currency.append(line)
+            else:
+                lines_diff_currency.append(line)
+
+            if to_break:
+                break
+
+        if currency_id == company_currency:
+            total_credit = sum(map(
+                lambda x: x.credit or 0.0, lines_same_currency))
+            total_debit = sum(map(
+                lambda x: x.debit or 0.0, lines_same_currency))
+        elif currency_id == line.currency_id.id:
+            total_credit = sum(map(
+                lambda x: x.credit and x.amount_currency or 0.0,
+                lines_same_currency))
+            total_debit = sum(map(
+                lambda x: x.debit and x.amount_currency or 0.0,
+                lines_same_currency))
+
+        remaining_amount = price
+        #voucher line creation
+        for line in lines_same_currency+lines_diff_currency:
+
+            if line.currency_id and currency_id == line.currency_id.id:
+                amount_original = abs(line.amount_currency)
+                amount_unreconciled = abs(line.amount_residual_currency)
+            else:
+                #always use the amount booked in the company currency as the basis of the conversion into the voucher currency
+                amount_original = currency_pool.compute(cr, uid, company_currency, currency_id, line.credit or line.debit or 0.0, context=context_multi_currency)
+                amount_unreconciled = currency_pool.compute(cr, uid, company_currency, currency_id, abs(line.amount_residual), context=context_multi_currency)
+            line_currency_id = line.currency_id and line.currency_id.id or company_currency
+
+            # Calculate amount to impute
+            amount = (line.id in move_lines_found) and min(abs(remaining_amount), amount_unreconciled) or 0.0
+            rs = self._get_voucher_line_vals(line, line_currency_id,
+                    amount, amount_original, amount_unreconciled)
+
+            remaining_amount -= rs['amount']
+
+            #in case a corresponding move_line hasn't been found, we now try to assign the voucher amount
+            #on existing invoices: we split voucher amount by most old first, but only for lines in the same currency
+            disable_autoimpute = False
+            if ttype == 'receipt':
+                disable_autoimpute = self.pool['res.users'].has_group(
+                            cr, uid, 'l10n_ar_account_payment.'
+                            'group_disable_auto_impute_receipts')
+            elif ttype == 'payment':
+                disable_autoimpute = self.pool['res.users'].has_group(
+                            cr, uid, 'l10n_ar_account_payment.'
+                            'group_disable_auto_impute_payments')
+
+            if not move_lines_found:
+                if currency_id == line_currency_id:
+                    if not disable_autoimpute:
+                        # Auto reimpute
+                        total_debit, total_credit = self._try_autoimpute(
+                                rs, line, amount_unreconciled,
+                                total_debit, total_credit)
+
+            if rs['amount_unreconciled'] == rs['amount']:
+                rs['reconcile'] = True
+
+            if rs['type'] == 'cr':
+                default['value']['line_cr_ids'].append(rs)
+            else:
+                default['value']['line_dr_ids'].append(rs)
+
+        # NOTE: Indented down to avoid recomputation of writeoff in every loop
+        if len(default['value']['line_cr_ids']) > 0:
+            default['value']['pre_line'] = 1
+        elif len(default['value']['line_dr_ids']) > 0:
+            default['value']['pre_line'] = 1
+        default['value']['writeoff_amount'] = self._compute_writeoff_amount(cr, uid, default['value']['line_dr_ids'], default['value']['line_cr_ids'], price, ttype)
         return default
 
+
 account_voucher()
+
+# NOTE: Patching recompute_voucher_lines to insert some hooks
+origin = getattr(account_voucher_orig, 'recompute_voucher_lines')
+setattr(account_voucher_orig, 'recompute_voucher_lines', account_voucher_orig.recompute_voucher_lines)
 
 class account_voucher_line(osv.osv):
     _name = 'account.voucher.line'
