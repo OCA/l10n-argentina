cd06621e0947b85de1dc88be45a37979cec427e4
Santiago J. Said <said.santiago@gmail.com>
2018-08-02 09:51:46 -0300

    [FIX] Issue #49: no warehouse set in extra moves

--- l10n_ar_point_of_sale/stock.py
+++ l10n_ar_point_of_sale/stock.py
@@ -38,7 +38,7 @@ class stock_picking(models.Model):
         fiscal_position_id = res['fiscal_position']
         if not fiscal_position_id:
             raise osv.except_osv(_('Error'),
-                                 _('The order hasn\'t got Fiscal Position configured.')) 
+                                 _('The order hasn\'t got Fiscal Position configured.'))
         reads = fiscal_pos_obj.read(cr, uid, fiscal_position_id, ['denomination_id', 'denom_supplier_id'], context=context)
 
         # Es de cliente
@@ -49,13 +49,20 @@ class stock_picking(models.Model):
             res_pos = pos_ar_obj.search(cr, uid,[('shop_id', '=', move.warehouse_id.id), ('denomination_ids', 'in', denomination_id)])
             if not res_pos:
                 raise osv.except_osv( _('Error'),
-                                   _('You need to set up a Point of Sale in your Warehouse')) 
+                                   _('You need to set up a Point of Sale in your Warehouse'))
             pos_ar_id = res_pos[0]
         else:
             denomination_id = reads['denom_supplier_id'][0]
 
 
         res.update({'denomination_id' : denomination_id , 'pos_ar_id': pos_ar_id})
+        return res
+
+    @api.model
+    def _prepare_values_extra_move(self, op, product, remaining_qty):
+        res = super(stock_picking, self)._prepare_values_extra_move(op, product, remaining_qty)
+        warehouse_id = op.picking_id.picking_type_id.warehouse_id.id
+        res['warehouse_id'] = warehouse_id
         return res
 
 stock_picking()
