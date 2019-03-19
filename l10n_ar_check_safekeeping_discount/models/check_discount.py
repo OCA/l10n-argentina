4623449a132c1efd1f6c931ddcf588ddc0ef0a31
gbertolani <gaston.bertolani@gmail.com>
2019-03-19 17:26:38 -0300

    [FIX]No create moves without amount

--- l10n_ar_check_safekeeping_discount/models/check_discount.py
+++ l10n_ar_check_safekeeping_discount/models/check_discount.py
@@ -16,18 +16,19 @@ class CheckDiscount(models.Model):
 
     @api.multi
     def _prepare_check_line_vals_for_move(self):
+        res = super(CheckDiscount, self)._prepare_check_line_vals_for_move()
         other_checks = self.check_ids.filtered(
                 lambda x: x.state != 'safekeeped')
-        res = []
-        if other_checks:
-            res = super(CheckDiscount, self)._prepare_check_line_vals_for_move()
         safekeeped_checks = self.check_ids.filtered(
             lambda c: c.state == 'safekeeped')
-        wc_vals = {
-            'account_id': self.check_config_id.safekept_account_id.id,
-            'credit': sum(safekeeped_checks.mapped('amount')),
-            'debit': 0.0,
-            'currency_id': self.currency_id.id,
-        }
-        res.append(wc_vals)
+        if not other_checks:
+            res = []
+        if safekeeped_checks:
+            {
+                'account_id': self.check_config_id.safekept_account_id.id,
+                'credit': sum(safekeeped_checks.mapped('amount')),
+                'debit': 0.0,
+                'currency_id': self.currency_id.id,
+            }
+            res.append(wc_vals)
         return res
