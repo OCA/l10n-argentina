a1852f063375b3aef6da652b27552bce98c84564
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-10 13:50:58 +0100

    [FIX] base_vat_ar: RST syntax

--- base_vat_ar/__openerp__.py
+++ base_vat_ar/__openerp__.py
@@ -28,14 +28,15 @@
     "author": "E-MIPS,Odoo Community Association (OCA)",
     "category": "VAT Modules",
     "description": """
-    This module provide :
-    * VAT Check for Argentina.
-    * Modification of check_vat_routine:
-        The original routine, got the VAT Number an extract the first two letters
-        and used them to as Country ISO Code to know the country of the partner and to call
-        the routine related to this country.
-        This module modify this, getting the country from the partner itself (the ISO Code has
-        to be well defined) and if this is not defined, the original routine is executed.
+This module provides:
+
+* VAT Check for Argentina.
+* Modification of check_vat_routine:
+  The original routine, got the VAT Number an extract the first two letters
+  and used them to as Country ISO Code to know the country of the partner and to call
+  the routine related to this country.
+  This module modify this, getting the country from the partner itself (the ISO Code has
+  to be well defined) and if this is not defined, the original routine is executed.
     """,
     'data': [
         'partner_view.xml',
