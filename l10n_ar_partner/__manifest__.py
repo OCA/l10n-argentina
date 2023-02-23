6c19b53893c062f07e35830f47910207dc4e6683
Ignacio Buioli <ibuioli@gmail.com>
2023-02-23 12:52:58 -0300

    [IMP] [ADD] l10n_ar_partner

---
+++ l10n_ar_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# For copyright and license notices, see __manifest__.py file in module root
+# directory or check the readme files
+
+{
+    "name": "Extra Partner Data and Padrón for Argentina",
+    "version": "14.0.0.0.1",
+    "category": "Partner",
+    "license": "AGPL-3",
+    "summary": "Extra Partner Data and Padrón for Argentina",
+    "author": "Odoo Community Association (OCA), Codize, Exemax",
+    "website": "https://github.com/OCA/l10n-argentina",
+    "depends": ["base", "l10n_ar_afipws"],
+    "external_dependencies": {},
+    "data": ["views/res_partner.xml"],
+    "maintainers": ["nimarosa", "ibuioli"],
+    "installable": True,
+}
---
+++ l10n_ar_partner/readme/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+Extra Partner Data and Padrón for Argentina
+
+This module adds extra data for Argentina partner and AFIP Padrón A5.
+
+In order to access the online database of each taxpayer, it is necessary to validate the computer.
+In AFIP Portal go to "Administrador de Relaciones", then select the company in the dropdown "Autoridad de Aplicación".
+Click on "Adherir Servicio" button. Search the service on AFIP > WebServices and select "Servicio Consulta Padron A5".
+On "Incorporar nueva Relación" click the button "Buscar" of "Representante" and then select the computer on "Computador Fiscal" dropdown.
+Click the "Confirmar" button for end.
