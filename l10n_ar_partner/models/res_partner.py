f94db755b24dea5ba5fc02fd68cdda37ad49b5a9
Ignacio Buioli <ibuioli@gmail.com>
2023-02-27 10:00:36 -0300

    [IMP] [FIX] l10n_ar_partner

--- l10n_ar_partner/models/res_partner.py
+++ l10n_ar_partner/models/res_partner.py
@@ -4,6 +4,19 @@ from odoo import models
 
 _logger = logging.getLogger(__name__)
 
+RESP_IVA_MAPPING = {
+    "N": "Responsable Monotributo",
+    "NI": "Responsable Monotributo",
+    "AC": "IVA Responsable Inscripto",
+    "S": "IVA Responsable Inscripto",
+    "XN": "IVA No Alcanzado",
+    "AN": "IVA No Alcanzado",
+    "NA": "IVA No Alcanzado",
+    "EX": "IVA Sujeto Exento",
+}
+
+STATES = {"NEUQUEN": "Neuquén", "CORDOBA": "Córdoba"}
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
@@ -14,31 +27,21 @@ class ResPartner(models.Model):
 
         if self.l10n_latam_identification_type_id.name == "CUIT":
             ws_sr_padron_a5 = company_id.get_connection("ws_sr_padron_a5").connect()
-            # connect = ws_sr_padron_a5.Consultar('20000000516') # Testing
+            # ws_sr_padron_a5.Consultar('20000000516') # Testing
             ws_sr_padron_a5.Consultar(self.vat)
-            # _logger.info(ws_sr_padron_a5)
+
             self.name = ws_sr_padron_a5.denominacion
             self.street = ws_sr_padron_a5.direccion.capitalize()
             self.city = ws_sr_padron_a5.localidad.capitalize()
             self.zip = ws_sr_padron_a5.cod_postal
-            if ws_sr_padron_a5.tipo_persona == "FISICA":
-                self.company_type = "person"
-            else:
-                self.company_type = "company"
+            self.company_type = (
+                "person" if ws_sr_padron_a5.tipo_persona == "FISICA" else "company"
+            )
 
-            iva = ws_sr_padron_a5.imp_iva
             l10n_ar_type = ""
-
-            if iva == "N" or iva == "NI":
-                l10n_ar_type = "Responsable Monotributo"
-            elif iva == "AC" or iva == "S":
-                l10n_ar_type = "IVA Responsable Inscripto"
-            elif iva == "XN" or iva == "AN" or iva == "NA":
-                l10n_ar_type = "IVA No Alcanzado"
-            elif iva == "EX":
-                l10n_ar_type = "IVA Sujeto Exento"
-            else:
-                l10n_ar_type = "Consumidor Final"
+            l10n_ar_type = RESP_IVA_MAPPING.get(
+                ws_sr_padron_a5.imp_iva, "Consumidor Final"
+            )
 
             if l10n_ar_type != "":
                 iva_afip = self.env["l10n_ar.afip.responsibility.type"].search(
@@ -54,13 +57,9 @@ class ResPartner(models.Model):
                     self.country_id = country_id.id
 
                 provincia = ""
-
-                if ws_sr_padron_a5.provincia == "NEUQUEN":
-                    provincia = "Neuquén"
-                elif ws_sr_padron_a5.provincia == "CORDOBA":
-                    provincia = "Córdoba"
-                else:
-                    provincia = ws_sr_padron_a5.provincia
+                provincia = STATES.get(
+                    ws_sr_padron_a5.provincia, ws_sr_padron_a5.provincia
+                )
 
                 state_id = self.env["res.country.state"].search(
                     [("name", "like", provincia), ("country_id", "=", country_id.id)],
