ae959418ded623bc15f575701fa56ad28a63c8a6
nicolasrsande <nicolasrsande@gmail.com>
2023-11-11 22:27:38 -0300

    [ADD] l10n_ar_afipws

---
+++ l10n_ar_afipws/models/__init__.py
@@ -0,0 +1,8 @@
+# For copyright and license notices, see __manifest__.py file in module root
+# directory or check the readme files
+
+from . import afipws_certificate_alias
+from . import afipws_certificate
+from . import afipws_connection
+from . import res_company
+from . import res_config_settings
